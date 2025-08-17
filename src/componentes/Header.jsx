aa62509207bf09ad44e8a16e2e314bd448419a80
Ale Bavaro - Ale Gondra <bavaroalejandro@gmail.com>
2025-08-17 12:18:44 -0300

    mejore varias cosas

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -8,47 +8,98 @@ const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const location = useLocation();
 
-  const handleToggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
-
   const guestLinks = [
-    { path: "/", icon: <BsHouseDoor />, label: "Inicio" },
-    { path: "/contacto", icon: <BsHouseDoor />, label: "Contacto" },
-    { path: "/itinerario", icon: <BsClockFill />, label: "Itinerario" },
-    { path: "/tareas-boda", icon: <BsListCheck />, label: "Tareas Boda" }
+    { 
+      path: "/", 
+      icon: <BsHouseDoor />, 
+      label: "Inicio",
+      shortLabel: "Inicio",
+      tooltip: "Volver a la página principal"
+    },
+    { 
+      path: "/contacto", 
+      icon: <BsHouseDoor />, 
+      label: "Contacto con los Novios",
+      shortLabel: "Contacto",
+      tooltip: "Escríbenos si tienes alguna duda o necesitas información adicional"
+    },
+    { 
+      path: "/itinerario", 
+      icon: <BsClockFill />, 
+      label: "Itinerario de la Boda",
+      shortLabel: "Horarios",
+      tooltip: "Consulta los horarios y actividades planeadas para nuestra celebración"
+    },
+    { 
+      path: "/tareas-boda", 
+      icon: <BsListCheck />, 
+      label: "Tareas de la Boda",
+      shortLabel: "Tareas",
+      tooltip: "Lista de tareas pendientes para la organización de la boda"
+    }
   ];
+
+  const handleToggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMobileMenuOpen(false);
+  };
 
   return (
     <header className="app-header">
+      {/* Decoración superior */}
       <div className="header-decoration-top"></div>
-      <Navbar expand="lg" className="header-navbar">
+
+      {/* Barra de navegación principal */}
+      <Navbar expand="lg" className="header-navbar" expanded={isMobileMenuOpen}>
         <Container className="header-container">
+          {/* Logo */}
           <Navbar.Brand as={Link} to="/" className="header-logo">
-            <img src="../../public/img/02-logos/logolistastareaseventos.png" alt="Logo" className="logo-image" />
+            <img 
+              src="../../public/img/02-logos/logolistastareaseventos.png" 
+              alt="Logo" 
+              className="logo-image" 
+            />
           </Navbar.Brand>
+
+          {/* Botón hamburguesa para móvil */}
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
-            onClick={handleToggleMobileMenu}
             className="menu-toggle"
+            onClick={handleToggleMobileMenu}
           >
             <BsList className="menu-icon" />
           </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav" className={`navbar-menu ${isMobileMenuOpen ? "open" : ""}`}>
-            <Nav className="nav-links">
+
+          {/* Menú de navegación */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="quick-access-buttons">
               {guestLinks.map((link) => (
-                <Link
+                <Nav.Link
                   key={link.path}
+                  as={Link}
                   to={link.path}
-                  onClick={() => setIsMobileMenuOpen(false)}
-                  className={`nav-link ${location.pathname === link.path ? 'active' : ''}`}
+                  onClick={handleLinkClick}
+                  className={`nav-button ${
+                    location.pathname === link.path ? "active" : ""
+                  }`}
+                  title={link.tooltip}
+                  data-label={link.label}
                 >
-                  <span className="nav-icon">{link.icon}</span>
-                  <span className="nav-label">{link.label}</span>
-                </Link>
+                  <span className="icon">{link.icon}</span>
+                  <span className="nav-label">
+                    {window.innerWidth < 768 ? link.label : link.shortLabel}
+                  </span>
+                </Nav.Link>
               ))}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* Decoración inferior */}
       <div className="header-decoration-bottom"></div>
     </header>
   );
